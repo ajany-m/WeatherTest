b396a31b2e497d25b69257addf2ac99dda205c7c
ajany-m <ajany.m@gmail.com>
2024-07-29 19:03:58 +0300

    Create WeatherActivity

--- app/src/main/java/com/example/weathertest/MainActivity.kt
+++ app/src/main/java/com/example/weathertest/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.weathertest
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textView = findViewById<TextView>(R.id.tvTemperature)
+        val btnWeather = findViewById<Button>(R.id.btnWeather)
+        btnWeather.setOnClickListener {
+            val intent = Intent(this, WeatherActivity::class.java).also(::startActivity)
         }
     }
+
 }
---
+++ app/src/main/java/com/example/weathertest/WeatherActivity.kt
@@ -0,0 +1,15 @@
+package com.example.weathertest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_weather)
+
+    }
+
+}
